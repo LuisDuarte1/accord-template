936739365596b4e8f4e507aa8b1cfc5a1ecf1d83
Luís Duarte <lduarte@cloudflare.com>
2025-04-10 15:47:35 +0100

    feat: add D1 and react-query

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -3,14 +3,20 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { UserProvider } from "./context/User.tsx";
 import UsernamePopover from "./UsernamePopover.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Create a client
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <UserProvider>
-        <header className="flex justify-between items-center">
-          <UsernamePopover />
-        </header>
-        <App />
-    </UserProvider>
-  </StrictMode>,
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <UserProvider>
+                <header className="flex justify-between items-center">
+                    <UsernamePopover />
+                </header>
+                <App />
+            </UserProvider>
+        </QueryClientProvider>
+    </StrictMode>,
 );
